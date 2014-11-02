3411a1b173a7ec6f88cd74c8efad3828ec8ba358
Nico <laranicolas@gmail.com>
2014-11-02 12:38:45 -0300

    Revert "Add devise gem."
    This reverts commit 56434c69cfcc8f51f5b6fdf05f1fee1d1d07cfce.
    
    Conflicts:
    	Gemfile
    	Gemfile.lock
    	app/controllers/application_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-
   namespace :api do
     resources :users, :comments, :blog_posts
   end
@@ -9,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
