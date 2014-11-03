14b66c6be54c92da8167e4b9ece4f8febf9bd250
Nico <laranicolas@gmail.com>
2014-11-03 02:01:06 -0300

    Add authentication by token.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users, :comments, :blog_posts
+
+    post 'sign_up', to: 'users#create'
+    post 'sign_in', to: 'sessions#create'
+    delete 'sign_out', to: 'sessions#destroy'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
