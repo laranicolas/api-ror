d6d9adb870e02231b0ce4a09771f10e81e12d27b
Nico <laranicolas@gmail.com>
2014-11-03 02:01:06 -0300

    Add authentication by token.

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+module Api
+	class SessionsController < ApplicationController
+		wrap_parameters :user
+
+		skip_before_filter :authenticate_token!, only: [:create]
+
+		def create
+			if params[:email] && params[:password]
+				@current_user = User.authenticate(params[:email], params[:password])
+				head :unauthorized unless @current_user
+			else
+				head :bad_request
+			end
+		end
+
+		def destroy
+			@current_user.reset_token!
+			head :none_content
+		end
+	end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,49 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :null_session
+	respond_to :json
+
+	before_filter :authenticate_token!
+	skip_before_filter :verify_authenticity_token
+
+	def routing_error
+		raise ActionController::RoutingError.new("No route matches #{params[:path]}")
+	end
+
+	if Rails.env.development?
+		rescue_from Exception, with: :render_error
+	end
+
+	protected
+
+		def current_user=(user)
+			@current_user = user
+		end
+
+		def current_user
+			@current_user
+		end
+
+		def authenticate_token!
+			authenticate || render_unauthorized
+		end
+
+		def authenticate
+			authenticate_with_http_token do |token, options|
+				@current_user = User.find_by_token(token)
+			end
+		end
+
+	private
+
+		def render_unauthorized
+			render json: { error: 'Authentication Required' }.to_json, status: :unauthorized
+		end
+
+		def render_forbidden(exception)
+			render json: { error: exception.message }.to_json, status: :forbidden
+		end
+
+		def render_error(exception)
+			render json: { error: exception.message, baktrace: exception.backtrace }.to_json, status: :internal_server_error
+		end
 end
